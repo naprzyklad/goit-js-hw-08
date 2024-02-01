a21c163289b15c34372b5a1932efeb21ec639322
naprzyklad <mk18@spoko.pl>
2024-02-01 22:28:49 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItem = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+gallery.innerHTML = galleryItem;
+
+const simple = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'outside',
+  captionDelay: 250,
+});
